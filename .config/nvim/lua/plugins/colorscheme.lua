7ae3bfa4be69054093bbedf158c6fafa3cc447d9
jonny <jonny@japeto.ai>
2024-01-19 15:22:42 +0000

    nvim config

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  { "tomasiser/vim-code-dark" },
+  { "loctvl842/monokai-pro.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "codedark",
+      "monokai-pro",
+      "gruvbox",
+    },
+  },
+}
