b74112dbce59e7769d5d92b5fa40b14062fd6b85
Jonny Coddington <bottlerocketjonny@protonmail.com>
2024-03-17 16:22:07 +0000

    changed some stuff

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
+vim.opt.listchars = {
+  tab = "> ",
+  trail = " ",
+  nbsp = "+",
+}
+
 return {
   { "rcarriga/nvim-notify", enabled = false },
 }
