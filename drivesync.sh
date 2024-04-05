f8f0d2efe1c0cae4a09d73885f3187ec7deffa22
Jonny Coddington <bottlerocketjonny@protonmail.com>
2024-04-05 16:29:13 +0100

    changed some stuff

--- drivesync.sh
+++ drivesync.sh
@@ -3,13 +3,37 @@
 local_file="$HOME/Documents/Passwords.kdbx"
 remote_dir="gdrive:"
 file_name="Passwords.kdbx"
+remote_path="${remote_dir}${file_name}"
 
-if [ ! -f "$local_file" ]; then
-	echo "Local file does not exist. Attempting to download from Google Drive."
-	rclone copy "$remote_dir/$file_name" "$(dirname "$local_file")"
+# Fetch local file modification time as a Unix timestamp
+if [ -f "$local_file" ]; then
+	local_mod_time=$(date -r "$local_file" +%s)
 else
-	echo "Local file exists. Checking for updates to upload."
+	local_mod_time=0
+fi
+
+# Fetch remote file details
+remote_file_info=$(rclone lsl "$remote_path" | awk '{print $1, $2, $3}')
+if [[ -z "$remote_file_info" ]]; then
+	remote_mod_time=0
+else
+	# Parsing the remote file modification time
+	remote_date=$(echo "$remote_file_info" | awk '{print $3}')
+	remote_time=$(echo "$remote_file_info" | awk '{print $2}')
+	# Assuming the date format is YYYY-MM-DD and time format is HH:MM:SS
+	remote_mod_time=$(date -d "$remote_date $remote_time" +%s)
+fi
+
+# Compare modification times and sync accordingly
+if [ "$local_mod_time" -eq 0 ]; then
+	echo "Local file does not exist. Attempting to download from Google Drive."
+	rclone copy "$remote_path" "$(dirname "$local_file")"
+elif [ "$local_mod_time" -gt "$remote_mod_time" ]; then
+	echo "Local file is newer. Uploading to Google Drive."
 	rclone copy "$local_file" "$remote_dir"
-	echo "Checking for newer remote file to download."
-	rclone copy "$remote_dir/$file_name" "$(dirname "$local_file")"
+elif [ "$local_mod_time" -lt "$remote_mod_time" ]; then
+	echo "Remote file is newer. Downloading to local."
+	rclone copy "$remote_path" "$(dirname "$local_file")"
+else
+	echo "Files are synchronized. No action needed."
 fi
