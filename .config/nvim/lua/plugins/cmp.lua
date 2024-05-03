b8b50da7fe8db28008f877f043bc15e1b40e89f6
Jonny Coddington <bottlerocketjonny@protonmail.com>
2024-05-03 15:13:50 +0100

    -

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -35,7 +35,7 @@ return {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
       {
-        'hrsh7th/cmp-tabnine',
+        'tzachar/cmp-tabnine',
         build = './install.sh',
         dependencies = 'hrsh7th/nvim-cmp',
       },
