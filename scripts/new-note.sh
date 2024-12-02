87b232afbc506d807d2099e1031711e5e3793d6b
jonny <jonny@japeto.ai>
2024-12-02 11:17:11 +0000

    -

--- scripts/new-note.sh
+++ scripts/new-note.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
 notes_dir="$HOME/notes"
-
 mkdir -p "$notes_dir"
-
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 
-note_file="${notes_dir}/note_${timestamp}.md"
+if [ $# -eq 0 ]; then
+    note_file="${notes_dir}/note_${timestamp}.md"
+else
+    filename="$1"
+    filename="${filename%.md}"
+    note_file="${notes_dir}/${filename}_${timestamp}.md"
+fi
 
 nvim +"NoNeckPain" "$note_file"
